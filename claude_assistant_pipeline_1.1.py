52badc1b2f1e33bc34f080fe057a8b8748f512f0
Mauricio <maurocamacho@gmail.com>
2024-06-28 15:24:36 -0400

    Create claude_assistant_pipeline_1.1.py

---
+++ claude_assistant_pipeline_1.1.py
@@ -0,0 +1,84 @@
+"""
+title: IQ-claude V.1
+requirements: langchain langchain-community langchain-chroma langchain-anthropic langchain-openai unstructured psycopg2-binary
+"""
+
+import bs4
+from langchain import hub
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import S3DirectoryLoader
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
+
+from google.colab import userdata
+aws_access_key = userdata.get('aws_access_key_id')
+aws_secret_access_key = userdata.get('aws_secret_access_key')
+os.environ["OPENAI_API_KEY"] = userdata.get('openai')
+os.environ["ANTHROPIC_API_KEY"] = userdata.get('anthropic')
+bucket = 'roiq-screenshots'
+
+from langchain_anthropic import ChatAnthropic
+llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+
+class Pipeline:
+    def __init__(self):
+        self.name = "IQ-claude V.1"        
+        pass
+
+    async def on_startup(self):
+        # This function is called when the server is started.
+        print(f"on_startup:{__name__}")
+        pass
+
+    async def on_shutdown(self):
+        # This function is called when the server is stopped.
+        print(f"on_shutdown:{__name__}")
+        pass
+
+    def pipe(
+        self, user_message: str, model_id: str, messages: List[dict], body: dict
+    ) -> Union[str, Generator, Iterator]:
+        # 1. Load, chunk and index the contents of the blog to create a retriever.
+        loader = S3DirectoryLoader(
+            bucket=bucket,
+            prefix="nc5vknnz/md/",
+            aws_access_key_id=aws_access_key,
+            aws_secret_access_key=aws_secret_access_key
+        )
+        docs = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        splits = text_splitter.split_documents(docs)
+        vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+        retriever = vectorstore.as_retriever()
+
+
+        # 2. Incorporate the retriever into a question-answering chain.
+        system_prompt = (
+            "You are an assistant for question-answering tasks. "
+            "Use the following pieces of retrieved context to answer "
+            "the question. If you don't know the answer, say that you "
+            "don't know. Use three sentences maximum and keep the "
+            "answer concise."
+            "\n\n"
+            "{context}"
+        )
+
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system_prompt),
+                ("human", "{input}"),
+            ]
+        )
+
+        question_answer_chain = create_stuff_documents_chain(llm, prompt)
+        rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+        try:
+          response = rag_chain.invoke({"input": user_message})
+          return response['answer']
+        except Exception as e:
+            return f"Error: {e}"
